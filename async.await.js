7d55cbc5eed631e75615badd8a5e8abdce949c54
Iskander <grinerplaydark@gmail.com>
2021-05-31 17:23:37 +0300

    new async/await

--- async.await.js
+++ async.await.js
@@ -1,5 +1,34 @@
-console.log('start')
+const delay = (ms) => {
+    return new Promise(resolve => setTimeout(() => resolve(), ms))
+}
 
-window.setTimeout(function(){
-    console.log('timeout')
-},2000)
+delay(2000).then(result => console.log('2 seconds'))
+
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+function fetchTodos() {
+    return delay(2000).then(() => {
+        return fetch(url)
+    }).then(res => res.json())
+}
+
+fetchTodos().then(data => console.log(data)).catch(error => console.log(error))
+
+
+//with async/await
+
+//await === обработка promise
+async function fetchAsyncTodos() {
+    try {
+        await delay(2000)
+        const res = await fetch(url)
+        return await res.json()
+    } catch (err) {
+        if (err) {
+            console.log(err)
+        }
+    }
+
+}
+
+console.log(fetchAsyncTodos())
